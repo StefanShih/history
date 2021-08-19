5ab1cb6aa35194c316395ef426bc88e70c48ebb9
StefanShih <johin90106@gmail.com>
2021-08-19 10:25:05 +0800

    Add files via upload

--- test_GE/test_GE.js
+++ test_GE/test_GE.js
@@ -4,12 +4,12 @@ function send() {
   let name = document.querySelector('#name').value;
   let demand = document.querySelector('#demand').value;
   $.ajax({
-    type: 'POST',
     url: "https://script.google.com/macros/s/AKfycbwe9hS86buvGU7L5y1-03dpPn3ZvXNMaOU0ABF6hxfdtqR_lfJTc1ry1HOruHzR40lI/exec",
     data: {
         "name": name,
         "demand": demand
     },
+    type: "POST",
     success: function(response) {
       if(response == "成功"){
         alert("成功");
